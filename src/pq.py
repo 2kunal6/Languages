8d9b3718327f9f83c7245fa52899b5c5b0aa26fd
Kunal Rout <2kunal6@gmail.com>
2023-12-22 09:18:15 +0530

    iterating pq

--- src/pq.py
+++ src/pq.py
@@ -1,17 +1,30 @@
 from queue import PriorityQueue
 
-pq = PriorityQueue()
 
+def print_pq(pq):
+    for val in pq.queue:
+        print((val))
+
+    print((pq.qsize()))
+
+    while not pq.empty():
+        print(pq.get())
+
+    print((pq.qsize()))
+
+# second element is not considered
+# For these types use tuple or vector
+pq = PriorityQueue()
 pq.put('j', 1)
 pq.put('c', 1)
 pq.put('w', 1)
 pq.put('b', 1)
 pq.put('aj', 1)
 pq.put('p', 1)
+print_pq(pq)
 
-print((pq.qsize()))
+single_element_pq = PriorityQueue()
 
-while not pq.empty():
-    print(pq.get())
-
-print((pq.qsize()))
+single_element_pq.put(10)
+single_element_pq.put(100)
+print_pq(single_element_pq)
