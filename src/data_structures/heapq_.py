d6884dac8837eef5ccd397a96b582c4a40acf5ce
Kunal Rout <2kunal6@gmail.com>
2024-01-10 09:03:35 +0530

    priorityqueue class

--- src/data_structures/heapq_.py
+++ src/data_structures/heapq_.py
@@ -1,4 +1,9 @@
+# PriorityQueue is synchronized while heapq is not
+# runtime
+# maxheap: via heapq max_heapify, * -1, creating a new class (see __lt__)
 import heapq
+from queue import PriorityQueue
+
 
 c = []
 heapq.heappush(c, (2, 'x'))
@@ -9,3 +14,15 @@ heapq.heappush(c, (0, 'x'))
 heapq.heappush(c, (6, 'x'))
 
 print(c)
+
+pq = PriorityQueue()
+pq.put(2)
+pq.put(-1)
+pq.put(5)
+pq.put(100)
+pq.put(0)
+pq.put(6)
+
+while(not pq.empty()):
+    print(pq.get(), end = ' ')
+print()
